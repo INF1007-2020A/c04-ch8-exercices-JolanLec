035818de67faf22f2a6e3ed8c2a1d91aa54670d1
JolanLec <jolan.le-clainche@polymtl.ca>
2020-10-26 23:17:18 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -8,7 +8,7 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 # TODO: Définissez vos fonction ici
 def comparaison_fichier(file1, file2):
-    with open(file1, 'r', encoding= "utf-8")
+    with open(file1, 'r', encoding= "utf-8") as f 1
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
