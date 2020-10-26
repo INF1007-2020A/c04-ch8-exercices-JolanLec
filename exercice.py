69db96ed36e5fa90ddebd64647df05f1a9bbba5d
JolanLec <jolan.le-clainche@polymtl.ca>
2020-10-26 23:26:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -8,7 +8,7 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 # TODO: Définissez vos fonction ici
 def comparaison_fichier(file1, file2):
-
+    if file1 != file2:
         with open(file1, 'r', encoding= "utf-8") as f1, open(file2, 'r', encoding="urf-8") as f2:
             for index, line1 in enumerate(f1):
                 line2= f2.readline()
