978d6f5c7e668cfd75511064a380c1aa5e90a11e
JolanLec <jolan.le-clainche@polymtl.ca>
2020-10-27 22:05:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,7 +29,7 @@ def triple_les_espaces(input_file, output_file):
             # ou juste comme ca:
             # out_file.write(line.replace(" ", "   "))
 
-def note_lettre
+def note_lettre()
     
 
 
