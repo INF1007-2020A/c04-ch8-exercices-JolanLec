e16ae6b1151da38c0e4942a15f1166660bdba8b2
JolanLec <jolan.le-clainche@polymtl.ca>
2020-10-27 22:13:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,7 +29,7 @@ def triple_les_espaces(input_file, output_file):
             # ou juste comme ca:
             # out_file.write(line.replace(" ", "   "))
 
-def note_lettre(file_path,):
+def note_lettre(file_path, result_file):
     with open(file_path, 'r', encoding= "utf-8") as f_notes, open(correspondance.txt, 'w', encoding= "utf-8") as f_correspondance:
         for lines in notes:
             note = 
