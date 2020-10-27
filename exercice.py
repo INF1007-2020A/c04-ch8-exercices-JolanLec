110a4dfdf6520acece333d72db67d0ef3c1db188
JolanLec <jolan.le-clainche@polymtl.ca>
2020-10-27 22:17:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -35,7 +35,7 @@ def note_lettre(file_path, result_file_path):
 
     for note in notes_pourcentage:
         for key, value in PERCENTAGE_TO_LETTER.items():
-            if value[0] <= note <= value[]
+            if value[0] <= note <= value[1]
 
 
 
